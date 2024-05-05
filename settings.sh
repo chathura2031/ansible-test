d1278f1cf35a4420412ed8e9e1c361a74c6ade23
Chathura <chathura2031+github@outlook.com>
2024-05-05 15:45:10 +1000

    Added more settings configurations

--- settings.sh
+++ settings.sh
@@ -1,7 +1,12 @@
 # Set scrolling direction
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+
 # Set yakuake as the default terminal
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/yakuake
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
 # sudo apt remove gnome-terminal
 # sudo apt autoremove
+ln -s /etc/vim/.vimrc ~/.ideavimrc
+
+# Set the icon size for the dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
