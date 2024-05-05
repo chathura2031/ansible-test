b93a6df7b32dcb3dc704fc969218f00c138896d7
Chathura <chathura2031+github@outlook.com>
2024-05-05 16:04:55 +1000

    Updated yakuake install script

--- yakuake/install.sh
+++ yakuake/install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sudo apt install -y yakuake
-cp -v yakuakerc ~/.config/
+
+file_dir=$HOME"/.config"
+mkdir --parents $file_dir
+cp -v yakuakerc $file_dir
+
+# echo "yakuake" >> ~/.config/autostart
+ln -s /usr/share/applications/org.kde.yakuake.desktop $file_dir/autostart/yakuake.desktop
