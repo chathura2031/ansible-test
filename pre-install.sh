a12bd8a4773e489bdcf1ff2db2c53e393c5ac1c9
Chathura <chathura2031+github@outlook.com>
2024-09-07 20:12:27 +1000

    Add .NET runtime install script

--- pre-install.sh
+++ pre-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # TODO: Check last install time and only run if it has been more than a day or a force flag is provided
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+sudo apt autoremove -y
